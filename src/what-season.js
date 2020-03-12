f0944521a4ff9d2acf76d77e51cf8296de9812de
aidfromdeagland <aidfromdeagland@gmail.com>
2020-03-12 13:59:21 +0300

    feat: implement season identification

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,26 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+
+  if (!date) return 'Unable to determine the time of year!';
+
+  const seasonMap = {
+    0: 'winter',
+    1: 'winter',
+    2: 'spring',
+    3: 'spring',
+    4: 'spring',
+    5: 'summer',
+    6: 'summer',
+    7: 'summer',
+    8: 'autumn',
+    9: 'autumn',
+    10: 'autumn',
+    11: 'winter',
+  };
+
+  if (date && date instanceof Date && date.valueOf()) {
+    return seasonMap[date.getMonth()]
+  } else {
+    throw new Error("Don't make me laugh");
+  }
+
 };
