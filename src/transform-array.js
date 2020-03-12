d2075241c404ca239d7d8d15eb7570688cd8db6b
aidfromdeagland <aidfromdeagland@gmail.com>
2020-03-12 18:12:58 +0300

    feat: implement transform array func

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,30 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error('input should be an array');
+
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--discard-next':
+        i++;
+        break;
+      case '--double-next':
+        if (i !== arr.length - 1) {
+          result.push(arr[i + 1]);
+        }
+        break;
+      case '--discard-prev':
+        result.pop();
+        break;
+      case '--double-prev':
+        if (i !== 0) {
+          result.push(arr[i - 1]);
+        }
+        break;
+      default:
+        result.push(arr[i]);
+    }
+  }
+
+  return result;
 };
