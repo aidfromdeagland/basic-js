0bb4596603163d2d1a8a513aeb8c8d6bb0fb1341
aidfromdeagland <aidfromdeagland@gmail.com>
2020-03-12 10:51:54 +0300

    feat: implement carbon radiometric dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,11 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  const sampleActivityNumeric = parseFloat(sampleActivity);
+  if (typeof sampleActivity === 'string' && sampleActivityNumeric > 0 && sampleActivityNumeric < MODERN_ACTIVITY) {
+
+    return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivityNumeric) * HALF_LIFE_PERIOD / Math.log(2).toFixed(3) );
+  }
+    return false;
 };
