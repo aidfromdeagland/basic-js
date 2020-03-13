4f181f7077c17634337278ef849415a369872ff3
aidfromdeagland <aidfromdeagland@gmail.com>
2020-03-13 15:26:32 +0300

    feat: realize extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,21 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options = {}) {
+
+  if (typeof str !== 'string') {
+    str = str + '';
+  }
+
+  const defaultOptions = {
+      repeatTimes: 1,
+      separator: '+',
+      addition: '',
+      additionRepeatTimes: 1,
+      additionSeparator: '|'
+  };
+
+  return new Array(options.repeatTimes || defaultOptions.repeatTimes)
+    .fill((str + new Array(options.additionRepeatTimes || defaultOptions.additionRepeatTimes)
+    .fill('addition' in options ? options.addition + '' :  defaultOptions.addition)
+    .join(options.additionSeparator || defaultOptions.additionSeparator)))
+    .join(options.separator || defaultOptions.separator);
 };
   
