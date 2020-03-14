4b94b7390333684c4d7a41948e6ae1cf619649a8
aidfromdeagland <aidfromdeagland@gmail.com>
2020-03-14 11:51:30 +0300

    feat: implement vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,66 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+  constructor(directMode = true) {
+    this.isDirect = directMode;
+  }
+
+  get firstLetterCode() {
+    return 97
+  };
+
+  get lastLetterCode() {
+    return 122
+  };
+
+  get alphabetLength() {
+    return this.lastLetterCode - this.firstLetterCode + 1
+  };
+
+  encrypt(input, code) {
+    if (arguments.length < 2) {
+      throw new Error('pass the text and code for encryption')
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    let inputArr = input.toLowerCase().split('');
+    let codeArr = code.toLowerCase().split('');
+    let codeIndex = 0;
+    let result = inputArr.map((x) => {
+      if (x >= 'a' && x <= 'z') {
+        return String.fromCharCode(
+          this.firstLetterCode +
+          (x.charCodeAt(0) + codeArr[codeIndex++ % codeArr.length].charCodeAt(0)) %
+          this.firstLetterCode % this.alphabetLength
+        );
+      } else {
+        return x;
+      }
+    });
+    return this.isDirect ? result.join('').toUpperCase() : result.reverse().join('').toUpperCase();
+  };
+
+  decrypt(input, code) {
+    if (arguments.length < 2) {
+      throw new Error('pass the text and code for decryption')
     }
+
+    let inputArr = input.toLowerCase().split('');
+    let codeArr = code.toLowerCase().split('');
+    let codeIndex = 0;
+    let result = inputArr.map((x) => {
+      if (x >= 'a' && x <= 'z') {
+        return String.fromCharCode(
+          this.firstLetterCode +
+          (x.charCodeAt(0) >= codeArr[codeIndex % codeArr.length].charCodeAt(0) ?
+            x.charCodeAt(0) - codeArr[codeIndex++ % codeArr.length].charCodeAt(0)
+            : this.lastLetterCode + 1 + x.charCodeAt(0) - codeArr[codeIndex++ % codeArr.length].charCodeAt(0))
+          % this.firstLetterCode % this.alphabetLength
+        );
+      } else {
+        return x;
+      }
+    });
+    return this.isDirect ? result.join('').toUpperCase() : result.reverse().join('').toUpperCase();
+  };
+
 }
 
 module.exports = VigenereCipheringMachine;
